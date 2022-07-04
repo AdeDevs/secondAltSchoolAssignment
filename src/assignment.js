1338f4376c26c25a50e0293b316ddf8a6f4f1a04
AdeyemiAkinyemi <>
2022-07-04 20:34:59 +0000

    Updated

--- src/assignment.js
+++ src/assignment.js
@@ -13,10 +13,10 @@ const assignment = {};
  * @param {Array} arrayOfNumbers the array of numbers to sum
  * @returns number the sum of the numbers
  */
-function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+ function sumOfNumbers(arrayOfNumbers) {
+     return arrayOfNumbers.reduce((num, number) => num + number, 0);
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -27,10 +27,14 @@ function sumOfNumbers(arrayOfNumbers) {
  * @param {Array} arrayOfNumbers the array containing even or non-even numbers
  * @returns number the count of even numbers
  */
+
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+   return arrayOfNumbers.reduce(
+        (num, number) => num + (number % 2 === 0 ? 1 : 0),
+        0
+    );  
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
